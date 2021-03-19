8fc036a6ae6c71c73b54fdf967a5c10084acea90
Jose Manuel <jmdcarq89@gmail.com>
2021-03-19 17:09:36 +0100

    registerModel

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -3,8 +3,31 @@ import React from 'react';
 import {Container} from './styles'
 
 const ModelsWrapper: React.FC = ({children}) => {
+    const wrapperRef = useRef<HTMLDivElement>(null)
+
+    const [registeredModels, setRegisteredModels] = useState<CarModel[]>([])
+
+    const registerModel = useCallback((model: CarModel) => {
+        setRegisteredModels(state => [...state, { ...model }])
+      }, [])
+    
+      const unregisterModel = useCallback((modelName: string) => {
+        setRegisteredModels(state =>
+          state.filter(model => model.modelName !== modelName)
+        )
+      }, [])
+    
+      const getModelByName = useCallback(
+        (modelName: string) => {
+          return registeredModels.find(item => item.modelName === modelName) || null
+        },
+        [registeredModels]
+      )
+
     return (
-        <Container>
+        <Container
+            ref={wrapperRef}
+        >
             {children}
         </Container>
     )
